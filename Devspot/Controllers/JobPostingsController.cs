1c9265ac399206fe77ca07d564dfcac67e4ce1c6
robertgouveia <robertgouveia03@hotmail.com>
2024-11-28 22:24:58 +0000

    Updated UI

--- Devspot/Controllers/JobPostingsController.cs
+++ Devspot/Controllers/JobPostingsController.cs
@@ -1,11 +1,14 @@
+using Devspot.Constants;
 using Devspot.Models;
 using Devspot.Repositories;
 using Devspot.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Devspot.Controllers;
 
+[Authorize]
 public class JobPostingsController : Controller
 {
     private readonly IRepository<JobPosting> _repository;
@@ -20,12 +23,17 @@ public class JobPostingsController : Controller
         _userManager = userManager;
     }
 
+    [AllowAnonymous]
     public async Task<IActionResult> Index()
     {
-        var jobPostings = await _repository.GetAllAsync();
-        return View(jobPostings);
+        var allJobPostings= await _repository.GetAllAsync();
+        if (!User.IsInRole(Roles.Employer)) return View(allJobPostings);
+        
+        var filteredJobPostings = allJobPostings.Where(x => x.UserId == _userManager.GetUserId(User));
+        return View(filteredJobPostings);
     }
-
+    
+    [Authorize(Roles = $"{Roles.Admin}, {Roles.Employer}")]
     public IActionResult Create()
     {
         return View();
@@ -34,20 +42,29 @@ public class JobPostingsController : Controller
     [HttpPost]
     public async Task<IActionResult> Create(JobPostingViewModel vm)
     {
-        if (ModelState.IsValid)
+        if (!ModelState.IsValid) return View(vm);
+        
+        var posting = new JobPosting
         {
-            var posting = new JobPosting
-            {
-                Title = vm.Title,
-                Description = vm.Description,
-                Company = vm.Company,
-                Location = vm.Location,
-                UserId = _userManager.GetUserId(User)!, // User is from the Identity Package
-            };
-            await _repository.AddAsync(posting);
-            return RedirectToAction(nameof(Index));
-        }
+            Title = vm.Title,
+            Description = vm.Description,
+            Company = vm.Company,
+            Location = vm.Location,
+            UserId = _userManager.GetUserId(User)!, // User is from the Identity Package
+        };
+        await _repository.AddAsync(posting);
+        return RedirectToAction(nameof(Index));
+    }
+    
+    [Authorize(Roles = $"{Roles.Admin}, {Roles.Employer}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var jobPosting = await _repository.GetByIdAsync(id);
+        if (jobPosting is null) return NotFound();
 
-        return View(vm);
+        if (!User.IsInRole(Roles.Admin) && jobPosting.UserId !=  _userManager.GetUserId(User)) return Forbid();
+        
+        await _repository.DeleteAsync(jobPosting.Id);
+        return Ok();
     }
 }
